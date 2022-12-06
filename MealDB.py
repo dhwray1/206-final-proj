ce33422ced71db75438376a41983993bddd9fada
dhwray1 <111923848+dhwray1@users.noreply.github.com>
2022-12-05 20:07:38 -0500

    Add files via upload

--- MealDB.py
+++ MealDB.py
@@ -7,24 +7,38 @@ import os
 API_KEY = '1'
 url = "https://www.themealdb.com/api/json/v1/1/filter.php?"
 ingredient_list = ["chicken breast", "eggs", "sugar"]
+
 db_list = []
 for ingredient in ingredient_list:
-    ingredient_dict = {}
     param = {"i": ingredient}
     response = requests.get(url, params=param)
     #print(response.text)
     x=json.loads(response.text)
     for key in x:
         for dict in x[key]:
+            ingredient_dict = {}
             #print(dict)
             name = dict['strMeal']
             id = dict['idMeal']
             ingredient_dict[ingredient] =  [name, id]
             #print(db_list)
-            print(ingredient_dict)
+            #print(ingredient_dict)
             db_list.append(ingredient_dict)
     #print(x)
 print(db_list)
+long_ing_list = []
+for i in range(len(db_list)): 
+    my_new_string = str(db_list[i].keys())
+    my_new_string = my_new_string.strip("dict_keys(")
+    my_new_string = my_new_string.strip("['")
+    my_new_string = my_new_string.rstrip("'])")
+    long_ing_list.append(my_new_string)
+print(long_ing_list)
+
+    # final_db_dict = {}
+    # for i in range(len(db_list)):
+    #     final_db_dict[i] = db_list[i]
+    # print(final_db_dict)
 
 def setUpDatabase(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
@@ -34,15 +48,34 @@ def setUpDatabase(db_name):
 
 def create_meal_table(cur, conn):
     # cur.execute('DROP TABLE IF EXISTS Chicken')
-    cur.execute('CREATE TABLE IF NOT EXISTS "Meals"("id" INTEGER PRIMARY KEY, "Ingredient" TEXT, "strMeal" TEXT)')
-    for number in range(100):
-        id = number
-        # cur.execute('SELECT MAX id')
+    cur.execute('CREATE TABLE IF NOT EXISTS "Meals"("id" INTEGER PRIMARY KEY, "Ingredient" TEXT, "strMeal" TEXT, "MealID" INTEGER)')
+    # for number in range(100):
+    #     id = number
+    # count = 0
+    cur.execute("SELECT count(*) FROM Meals")
+    count = cur.fetchone()[0]
+        #for lst in dog_lst:
+            #for tup in lst:
+                #print(tup[0])
+                #cur.execute("INSERT INTO Dogs (breed, life_span, weight, height) VALUES (?,?,?,?)", (tup[0], tup[1], tup[2], tup[3]))
+                #count+=1
+        #cur.execute('SELECT MAX id')
+    num_count = 0
+    while count < len(long_ing_list) and num_count < 25:
+    # for index in range(len(long_ing_list)):
+        cur.execute("INSERT INTO Meals (id, Ingredient, strMeal, MealID) VALUES (?,?,?,?)", (count+1, long_ing_list[count], db_list[count][long_ing_list[count]][0], db_list[count][long_ing_list[count]][1]))
+        num_count += 1
+        count += 1
+         # count+=1
         # if returned value == none
         # count = 0
         # for smtg in range(count, count+25):
         # insert id 
-        conn.commit()
+  
+    conn.commit()
+
+    # if count == 25 or count == 50 or count == 75 or count == 100:
+        
 
 # def create_eggs_table(cur, conn):
 #     # cur.execute('DROP TABLE IF EXISTS Eggs')
@@ -55,11 +88,21 @@ def create_meal_table(cur, conn):
 #     conn.commit()
 
 
-
+#     cur.execute("CREATE TABLE IF NOT EXISTS Dogs (id INTEGER PRIMARY KEY, 'breed' TEXT, 'life_span' TEXT, 'weight' TEXT, 'height' TEXT)")
+#     dog_lst=print_dog()
+#     #print(dog_lst)
+#     #count=1
+#     for lst in dog_lst:
+#         for tup in lst:
+#             #print(tup[0])
+#             cur.execute("INSERT INTO Dogs (breed, life_span, weight, height) VALUES (?,?,?,?)", (tup[0], tup[1], tup[2], tup[3]))
+#             #count+=1
+#     conn.commit()
 
 def main():
     # SETUP DATABASE AND TABLE
     cur, conn = setUpDatabase('meals.db')
+    #create_db_list()
     create_meal_table(cur, conn)
     # create_eggs_table(cur, conn)
     # create_sugar_table(cur, conn)
