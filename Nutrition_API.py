ce131e2f8efa5abdc63e6dd1015d2db957ca04ea
anjfranc <97130501+anjfranc@users.noreply.github.com>
2022-12-08 12:14:04 -0500

    Add files via upload

--- Nutrition_API.py
+++ Nutrition_API.py
@@ -3,6 +3,9 @@ import json
 import unittest
 import sqlite3
 import os
+# import plotly.graph_objects as go
+# import numpy as np
+# import matplotlib.pyplot as plt
 
 lst=["butter" , "chicken", "eggs", "sugar", "salt", "bok choy", "drumstick", "tomato", "lemon", "potato",
  "spinach", "onion", "mushroom", "radish", "chives", "coriander", "paprika", "red chili", "saffron", "beef", "turkey", "salmon",
@@ -21,7 +24,7 @@ lst=["butter" , "chicken", "eggs", "sugar", "salt", "bok choy", "drumstick", "to
 url= "https://api.edamam.com/api/nutrition-data"
 final_lst=[]
 for item in lst:
-    param= {"app_id":"3b2f2a33", "app_key":"125bbe77c7affc11fa2e9496a74b6395", "ingr":item, "nutrition-type": "logging"}
+    param= {"app_id":"74681b40", "app_key":"3fc0acaaa7ef5d7154f435185594e712", "ingr":item, "nutrition-type": "logging"}
     response= requests.get(url, params= param)
     x=json.loads(response.text) #Dictionary
 
@@ -86,6 +89,42 @@ def create_carb_table(cur, conn):
         count += 1
     conn.commit()
 
+def calculating_data(cur, conn):
+    chicken_fat = cur.execute("SELECT Fat FROM Fat WHERE Ingredient = 'chicken'")
+    chicken_fat = cur.fetchone()
+    # print(chicken_fat)
+
+    chicken_protein  = cur.execute("SELECT Protein FROM Protein WHERE Ingredient = 'chicken'")
+    chicken_protein = cur.fetchone()
+    # print(chicken_protein)
+
+    chicken_carbs = cur.execute("SELECT Carbohydrates FROM Carbohydrates WHERE Ingredient = 'chicken'")
+    chicken_carbs = cur.fetchone()
+    # print(chicken_carbs)
+
+    chicken_fat_num= float(chicken_fat[0])
+    chicken_prot_num= float(chicken_protein[0])
+    chicken_carbs_num= float(chicken_carbs[0])
+
+    # Carbohydrates provide 4 calories per gram, protein provides 4 calories per gram, and fat provides 9 calories per gram.
+    total_calories= (chicken_fat_num*9)+(chicken_prot_num*4)+ (chicken_carbs_num*4)
+    print("The total calories for an expected serving size of chicken is " + str(total_calories))
+
+
+
+# def create_my_bargraph(): 
+#     # fig= go.FigureWidget(data=go.Bar( x=["Fat (in grams)", "Protein (in grams)", "Carbohydrates (in grams)"],  y=[22.22, 27.67, 13.42], color=['#58508d', '#bc5090', '#ff6361']))
+#     # # path=os.path.dirname(os.path.abspath(__file__))
+#     # # fig.write_image(os.path.join(path,'bar1.png'))
+#     # fig.show()
+#     height=[22.22, 27.67, 13.42]
+#     bars=["Fat (in grams)", "Protein (in grams)", "Carbohydrates (in grams)"]
+#     x_pos= np.arange(len(bars))
+#     plt.bar(x_pos, height, color=['#58508d', '#bc5090', '#ff6361'])
+#     plt.xticks(x_pos, bars)
+#     plt.show()
+
+    
 
 
 def main():
@@ -94,6 +133,8 @@ def main():
     create_fat_table(cur, conn)
     create_protein_table(cur, conn)
     create_carb_table(cur, conn)
+    calculating_data(cur, conn)
+    # create_my_bargraph()
 
 
 main()
